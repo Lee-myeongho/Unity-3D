e3a7d5c6a38b1ee4bbc095117b925f32dcbbdc8f
Lee-myeongho <lmh5559@naver.com>
2025-07-11 17:10:26 +0900

    7.11

--- Assets/01. Data Structure/02. Scripts/Hanoi Tower/HanoiTower.cs
+++ Assets/01. Data Structure/02. Scripts/Hanoi Tower/HanoiTower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HanoiTower : MonoBehaviour
 {
@@ -11,12 +12,17 @@ public class HanoiTower : MonoBehaviour
     public BoardBar[] bars; // L, C, R
 
     public TextMeshProUGUI countTextUI;
+    public Button answerButton;
 
     public static GameObject selectedDonut;
     public static bool isSelected;
     public static BoardBar currBar;
     public static int moveCount;
 
+    private void Awake()
+    {
+        answerButton.onClick.AddListener(HanoiAnswer);
+    }
     IEnumerator Start()
     {
 
@@ -44,5 +50,27 @@ public class HanoiTower : MonoBehaviour
         }
 
         countTextUI.text = moveCount.ToString();
+    }
+
+    public void HanoiAnswer()
+    {
+        HanoiRoutine((int)hanoiLevel, 0, 1, 2 );
+    }
+
+    private void HanoiRoutine(int n, int from, int temp, int to)
+    {
+        if (n == 0)
+            return;
+        if (n == 1)
+        {
+            Debug.Log($"{n}번째 도넛을 {from}에서 {to}로 이동");
+        }
+        else
+        {
+            HanoiRoutine(n - 1, from, to, temp);
+            Debug.Log($"{n}번째 도넛을 {from}에서 {to}로 이동");
+            HanoiRoutine(n - 1, temp, from, to);
+        }
+        
     }
 }
